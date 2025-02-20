b47aae72b21eb68c7548688eaa322a736be1076f
c-ehrlich <ehrlich.christopher@gmail.com>
2025-02-20 10:54:51 +0700

    next-axiom example logging from trpc

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,21 +1,22 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { type NextRequest } from "next/server";
 
 import { env } from "~/env";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
+import { withAxiom, type AxiomRequest } from "next-axiom";
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a HTTP request (e.g. when you make requests from Client Components).
  */
-const createContext = async (req: NextRequest) => {
+const createContext = async (req: AxiomRequest) => {
   return createTRPCContext({
     headers: req.headers,
+    log: req.log,
   });
 };
 
-const handler = (req: NextRequest) =>
+const handler = withAxiom((req: AxiomRequest) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
@@ -25,10 +26,11 @@ const handler = (req: NextRequest) =>
       env.NODE_ENV === "development"
         ? ({ path, error }) => {
             console.error(
-              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
             );
           }
         : undefined,
-  });
+  }),
+);
 
 export { handler as GET, handler as POST };
